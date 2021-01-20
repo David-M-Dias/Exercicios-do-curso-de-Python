37911ce0f9774c07d00a5d1478fb8c1c129d75d6
David Maxwell Dias <david.maxwell18@gmail.com>
2021-01-20 00:16:46 -0300

    atualizações

--- Mundo2/Desafio37.py
+++ Mundo2/Desafio37.py
@@ -2,7 +2,10 @@ from math import sqrt
 
 
 n = int(input("Digite um número: "))
-converte = int(input("Converter para:\n1. Binário \n2.Octal \n3.Hexadecimal \n\nDigite sua opção: "))
+#converte = int(input("Converter para:\n1. Binário \n2.Octal \n3.Hexadecimal \n\nDigite sua opção: "))
+numero = []
+resto = n % 2
+div = n // 2
 
-if converte == 1:
-    
+print(div)
+print(resto)
